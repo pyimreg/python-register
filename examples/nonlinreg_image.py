35e161ff041a1f17e9fb58f7d308444a93171369
Nathan Faggian <nathan.faggian@gmail.com>
2011-07-11 21:41:52 +1000

    Added another nonlinear image registration example, uses image data to test the non-linear registration code.

---
+++ examples/nonlinreg_image.py
@@ -0,0 +1,27 @@
+from matplotlib.pyplot import imread
+from register import register
+from register.visualize import plot
+
+# Load the smile and frown.
+image = imread('data/frown.png')[:, :, 0]
+template = imread('data/smile.png')[:, :, 0]
+
+# Apply a smoothing kernel to the image and template.
+image = register.smooth(image, 2.5)
+template = register.smooth(template, 2.5)
+
+spline = register.Register(
+    model='spline',
+    sampler='spline'
+    )
+
+# Turn that frown upside down.
+p, warp, img, error = spline.register(
+    image, 
+    template,
+    alpha=15,
+    verbose=True,
+    plotCB=plot.gridPlot
+    )
+
+plot.show()
