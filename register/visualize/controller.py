0e8228570d5408c056230ad72f9448d69807b0d5
Nathan Faggian <nfaggian@bom.gov.au>
2012-03-16 18:15:05 +1100

    Updates, removed graphics.py and starting playing around with a model-view-controller idea I had for user interaction.

--- register/visualize/controller.py
+++ register/visualize/controller.py
@@ -1,12 +1,56 @@
-""" Model view controller """
+""" Model view controller in QT """
 
 import sys
-import graphics
 
 from PyQt4 import QtCore, QtGui
 
+#==============================================================================
+# View (only ever draws the model data and retransmits messages)
+#==============================================================================
 
-class PointModel():
+
+class dialog(object):
+
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(648, 612)
+        Dialog.setModal(False)
+
+        self.graphicsView = QtGui.QGraphicsView(Dialog)
+        self.graphicsView.setGeometry(QtCore.QRect(10, 10, 631, 591))
+        self.graphicsView.setObjectName("graphicsView")
+
+        self.scene = QtGui.QGraphicsScene()
+        self.graphicsView.setScene(self.scene)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(
+            QtGui.QApplication.translate(
+                "Dialog",
+                "View",
+                None,
+                QtGui.QApplication.UnicodeUTF8
+                )
+            )
+
+    def draw(self, model):
+        """
+        Views a model as a point.
+        """
+        self.scene.addRect(
+            QtCore.QRect(model.x, model.y, 10, 10),
+            pen=QtGui.QPen()
+            )
+
+
+#==============================================================================
+# Model (data)
+#==============================================================================
+
+class Model():
     """
     Representation of things, like points and polygons.
     """
@@ -15,16 +59,19 @@ class PointModel():
         self.y = y
 
 
+#==============================================================================
+# Controller (subscribes to messages from view)
+#==============================================================================
+
+
 class Controller(QtGui.QDialog):
     """ Control of view and model """
 
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
 
-        self.view = graphics.Ui_Dialog()
+        self.view = dialog()
         self.view.setupUi(self)
-
-        self.model = PointModel(50, 50)
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
